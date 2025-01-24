704610209278e8e91431f5d8cd998937f0ca98f3
Maya <mayas2744@tuckahoeschools.org>
2025-01-24 13:32:23 +0000

    third commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+{
+    question: "What type of work environment do you prefer?",
+    choices: ["Office with a computer", "Art studio", "Science Lab", "Classroom"],
+    scores: [1,0,0,1]            //scores for Engineer, Artist, Scientist, and Teacher
+},
+{
+    question: "What do you enjoy doing the most?",
+    choices: ["Coding", "Drawing", "Solving Problems", "Helping Others"],
+    scores: [1,0,0,0]
+}
+]
